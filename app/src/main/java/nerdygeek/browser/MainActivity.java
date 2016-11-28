88fba444db44bebbd61717128d92c43bfd07fe7e
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-28 11:32:55 +0300

    Created android project

---
+++ app/src/main/java/nerdygeek/browser/MainActivity.java
@@ -0,0 +1,18 @@
+package nerdygeek.browser;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Project - Browser
+ * Package - nerdygeek.browser
+ * <p>
+ * Victor Mwenda
+ * +254(0)718034449
+ * vmwenda.vm@gmail.com
+ * <p>
+ * Android App Development Laptop
+ * Created by victor on 11/28/2016 at 11:26 AM.
+ */
+
+public class MainActivity extends AppCompatActivity {
+}
