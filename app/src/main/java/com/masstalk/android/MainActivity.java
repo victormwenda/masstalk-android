2da6013cc5eae4a69368e35f67f3fb794d1efc21
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-28 17:55:47 +0300

    Renamed the activity main to fragment main

--- app/src/main/java/com/masstalk/android/MainActivity.java
+++ app/src/main/java/com/masstalk/android/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
 
         initViews();
         initWebView();
