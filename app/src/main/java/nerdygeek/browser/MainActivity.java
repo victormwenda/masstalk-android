425450dd44fe648133e6a469695f8a3a44b3477e
Victor Mwenda <vmwenda.vm@gmail.com>
2016-11-28 15:56:09 +0300

    Removed method to stop execution(halt_compiler) until debugger makes a socket connection to the app

--- app/src/main/java/nerdygeek/browser/MainActivity.java
+++ app/src/main/java/nerdygeek/browser/MainActivity.java
@@ -3,7 +3,6 @@ package nerdygeek.browser;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Debug;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.ContentLoadingProgressBar;
@@ -44,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Debug.waitForDebugger();
+
         initViews();
         initWebView();
     }
